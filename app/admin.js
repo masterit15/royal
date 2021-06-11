430a7e89a4b633e222bb95537d468f064aea637c
masterit15 <masterit15@yandex.ru>
2021-06-11 17:21:32 +0300

    ee

--- app/admin.js
+++ app/admin.js
@@ -14,22 +14,23 @@ $(function() {
     }
   })
   function initMoreImages() {
-      if ($('input.field').val()) {}
-      let result = [...$('input.field').val().split(',').map(Number)]
-      $('.edition-selected').text('Выбрано: ' + result.length);
-      $('.frame label').on('click', function(e) {
-          $(this).toggleClass('checked')
-          if ($(this).hasClass('checked')) {
-              result.push(Number($(this).find('input:not(:checked)').val()))
-          } else {
-              result = result.filter(id => Number(id) !== Number($(this).children('input').val()))
-          }
-          result = result.filter(id => Number(id) !== 0)
-          $('.field').val(result.join(','));
-          $('.edition-selected').text('Выбрано: ' + result.length);
-          return false
-      })
-
+      if ($('input.field').val()) {
+        console.log($('input.field').val());
+        let result = [...$('input.field').val().split(',').map(Number)]
+        $('.edition-selected').text('Выбрано: ' + result.length);
+        $('.frame label').on('click', function(e) {
+            $(this).toggleClass('checked')
+            if ($(this).hasClass('checked')) {
+                result.push(Number($(this).find('input:not(:checked)').val()))
+            } else {
+                result = result.filter(id => Number(id) !== Number($(this).children('input').val()))
+            }
+            result = result.filter(id => Number(id) !== 0)
+            $('.field').val(result.join(','));
+            $('.edition-selected').text('Выбрано: ' + result.length);
+            return false
+        })
+      }
   }
   $('.load_more').on('click', function() {
       loadMoreImages($(this).data('url'))
